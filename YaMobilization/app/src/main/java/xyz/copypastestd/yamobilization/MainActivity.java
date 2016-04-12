acb94729475413182a21eef30b72d2ea7728c598
RomeoND <copypastestd@gmail.com>
2016-04-13 00:27:30 +0300

    recyclerView works

--- YaMobilization/app/src/main/java/xyz/copypastestd/yamobilization/MainActivity.java
+++ YaMobilization/app/src/main/java/xyz/copypastestd/yamobilization/MainActivity.java
@@ -1,13 +1,53 @@
 package xyz.copypastestd.yamobilization;
 
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SimpleRecyclerAdapter mSimpleRecyclerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+       /* getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
+        collapsingToolbarLayout.setTitle("Коты");*/
+
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+
+        recyclerView.setHasFixedSize(true);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        List<String> listData = new ArrayList<>();
+        listData.add("Мурзик");
+        listData.add("Барсик");
+        listData.add("Васька");
+        listData.add("Рыжик");
+        listData.add("Мурзик");
+        listData.add("Барсик");
+        listData.add("Васька");
+        listData.add("Рыжик");
+        listData.add("Мурзик");
+        listData.add("Барсик");
+        listData.add("Васька");
+        listData.add("Рыжик");
+
+        if (mSimpleRecyclerAdapter == null) {
+            mSimpleRecyclerAdapter = new SimpleRecyclerAdapter(listData);
+            recyclerView.setAdapter(mSimpleRecyclerAdapter);
+        } else {
+            mSimpleRecyclerAdapter.notifyDataSetChanged();
+        }
     }
 }
